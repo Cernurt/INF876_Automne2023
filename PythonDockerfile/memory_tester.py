06d3eedda8d62e5d88ccd8346c904890a1c68483
ryanmkgprojects <103485196+ryanmkgprojects@users.noreply.github.com>
2023-11-30 23:55:39 -0500

    Update memory_tester.py

--- PythonDockerfile/memory_tester.py
+++ PythonDockerfile/memory_tester.py
@@ -10,7 +10,7 @@ python_code = sys.argv[2]
 resource.setrlimit(resource.RLIMIT_AS, (memory_setting * 1024 * 1024, memory_setting * 1024 * 1024)) 
 
 start_time = time.time()
-subprocess.run(["scalene", python_code])
+subprocess.run(["python3", python_code])
 end_time = time.time()
 
 print(f"Timing program : {resource.getrusage(resource.RUSAGE_CHILDREN).ru_utime}")
